4247f572362a31d5309060c1953268593adec6f9
RaphaelVictor203 <37965272+RaphaelVictor203@users.noreply.github.com>
2021-10-13 22:30:06 -0300

    Mensagem customizada

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Flamengo é isso!!!"
+    return "Minha frase customizada criativa :)"
 
 if __name__ == '__main__':
     app.run(debug=True)
